8ed8b53fadfd0c31db9d97c2f479489e582df5eb
Johan Ussa <johanuss0405@gmail.com>
2023-03-17 17:02:19 -0500

    Intento opcion 1 again

--- index.js
+++ index.js
@@ -2,15 +2,14 @@
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const { ApolloServerPluginDrainHttpServer } = require('@apollo/server/plugin/drainHttpServer');
-const http = require('http');
 const dbConfig = require('./src/dbConfig/dbConfig');
 const resolvers = require('./src/graphQL/resolvers');
 const typeDefs = require('./src/graphQL/typeDefs');
 const mongoose = require('mongoose');
 const app = require('./app.js');
+const http = require('http');
 
 const httpServer = http.createServer(app);
-
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -24,16 +23,16 @@ mongoose.connect(dbConfig.dataBase, {
 .then(() => console.log('Data Base Connected'))
 .catch(err => console.log('Connect Error ', err));
 
-const start = async () => {
+// const start = async () => {
+//   await server.start();
+//   app.use('/', expressMiddleware(server));
+//   await new Promise (resolve => httpServer.listen(dbConfig.port, resolve));
+//   console.log(`🚀 Server ready at http://localhost:${dbConfig.port}`);
+// }
+// start();
+
+app.listen(dbConfig.port, async () => {
   await server.start();
   app.use('/', expressMiddleware(server));
-  await new Promise (resolve => httpServer.listen(dbConfig.port, resolve));
   console.log(`🚀 Server ready at http://localhost:${dbConfig.port}`);
-}
-start();
-
-// app.listen(dbConfig.port, async () => {
-//   await server.start();
-//   app.use(expressMiddleware(server));
-//   console.log(`🚀 Server ready at http://localhost:${dbConfig.port}`);
-// });
+});
